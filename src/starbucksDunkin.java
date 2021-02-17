225eee5710e40685eb80d49840fce0f2b8274b80
Humna Hanif <humnahanif@Humnas-MacBook-Pro.local>
2021-02-16 19:45:20 -0500

    case insensitive checker added

--- src/starbucksDunkin.java
+++ src/starbucksDunkin.java
@@ -5,9 +5,9 @@ public class starbucksDunkin {
 		Scanner choice = new Scanner(System.in);
 		System.out.println("Which is better: Starbucks or Dunkin?");
 		String opinion = choice.nextLine();
-		if (opinion.equals("starbucks")) {
+		if (opinion.toLowerCase().equals("starbucks")) {
 			System.out.println("Yes!");
-		} else if (choice.equals("dunkin")){
+		} else if (opinion.toLowerCase().equals("dunkin")) {
 			System.out.println("Um, you sure? I disagree.");
 		}
 	}
