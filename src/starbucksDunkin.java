992a6b04c77e411b9d82ef29720248968e280c59
Humna Hanif <humnahanif@Humnas-MacBook-Pro.local>
2021-02-16 20:47:53 -0500

    reponse to those who like Dunkin

--- src/starbucksDunkin.java
+++ src/starbucksDunkin.java
@@ -9,6 +9,21 @@ public class starbucksDunkin {
 			System.out.println("Yes!");
 		} else if (opinion.toLowerCase().equals("dunkin")) {
 			System.out.println("Um, you sure? I disagree.");
+			System.out.println("I think I can change your mind. Would you like to know why I hate Dunkin?");
+			Scanner response = new Scanner(System.in);
+			String why = response.nextLine();
+			
+			if (why.toLowerCase().equals("yes")) {
+				System.out.println("1. Dunkin uses tap water (yes TAP WATER!)");
+				System.out.println("2. Their iced coffee is too watery.");
+				System.out.println("3. They ALWAYS find a way to mess up someone's order.");
+				System.out.println("4. The menu is boring, not many options to choose from.");
+				System.out.println("5. The drinks have straight up sugar.");
+				System.out.println("I can keep going but if your not convinced already I don't know what to say.");
+				System.out.println("In conclusion, if you like Dunkin, your probably a kid who doesn't know what actual coffee tastes like (no offense). Goodbye now!");
+			} else if (opinion.toLowerCase().equals("no")) {
+				System.out.println("Alrighty then. Goodbye! And just so you know Starbucks is better.");
+			}
 		}
 	}
 }
